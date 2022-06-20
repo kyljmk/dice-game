5c9b14be4d754362e28c2e33cc2c2a72cdee10af
kyljmk <98695996+kyljmk@users.noreply.github.com>
2022-06-20 20:56:52 +0200

    add conditional to determine game winners

--- Program.cs
+++ Program.cs
@@ -7,11 +7,15 @@
             int playerRandom;
             int computerRandom;
 
+            int playerWinCount = 0;
+            int computerWinCount = 0;
+
             Random random = new Random();
 
             for (int i = 0; i < 10; i++)
             {
                 Console.WriteLine("Press Enter to roll the dice..");
+                Console.WriteLine();
 
                 Console.ReadKey();
 
@@ -19,10 +23,30 @@
                 Console.WriteLine($"You have rolled a {playerRandom}");
 
                 Console.WriteLine(". . . . .");
-                Thread.Sleep(3000);
+                Thread.Sleep(1000);
 
                 computerRandom = random.Next(0, 7);
                 Console.WriteLine($"The computer rolled a {computerRandom}");
+                Console.WriteLine();
+                
+                Thread.Sleep(1000);
+                if (playerRandom > computerRandom)
+                {
+                    Console.WriteLine("You won this one!");
+                    playerWinCount++;
+                }
+                else if (playerRandom == computerRandom)
+                {
+                    Console.WriteLine("This one is a draw!");
+                }
+                else 
+                {
+                    Console.WriteLine("You lost this one!");
+                    computerWinCount++;
+                }
+
+                Console.WriteLine();
+                Thread.Sleep(1000);
             }
         }
     }

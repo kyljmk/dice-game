3351e57163161f269b9e35b436519ae5ec1b8d93
kyljmk <98695996+kyljmk@users.noreply.github.com>
2022-06-20 20:22:05 +0200

    added player and computer dice rolls

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace DiceGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int playerRandom;
+            int computerRandom;
+
+            Random random = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Press Enter to roll the dice..");
+
+                Console.ReadKey();
+
+                playerRandom = random.Next(0, 7);
+                Console.WriteLine($"You have rolled a {playerRandom}");
+
+                Console.WriteLine(". . . . .");
+                Thread.Sleep(3000);
+
+                computerRandom = random.Next(0, 7);
+                Console.WriteLine($"The computer rolled a {computerRandom}");
+            }
+        }
+    }
+}
